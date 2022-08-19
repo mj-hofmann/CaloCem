16245a4dd91cdfb9cf71302178d6d5defe091557
mj-hofmann <matthias.j.hofmann@gmx.de>
2022-08-19 10:27:22 +0200

    csv extension removed from plot label

--- src/TAInstCalorimetry/tacalorimetry.py
+++ src/TAInstCalorimetry/tacalorimetry.py
@@ -9,7 +9,7 @@ import pandas as pd
 #
 # Base class of "ta-calorimetry"
 #
-class Measurement():
+class Measurement:
     """
     Base class of "tacalorimetry"
     """
@@ -35,10 +35,7 @@ class Measurement():
 
         # read
         if folder:
-            self.get_data_and_parameters_from_folder(
-                folder,
-                show_info=show_info
-            )
+            self.get_data_and_parameters_from_folder(folder, show_info=show_info)
         else:
             self._info = None
             self._data = None
@@ -70,15 +67,23 @@ class Measurement():
                 # collect information
                 try:
                     self._info = pd.concat(
-                        [self._info, self._read_calo_info_xls(file, show_info=show_info)])
+                        [
+                            self._info,
+                            self._read_calo_info_xls(file, show_info=show_info),
+                        ]
+                    )
                 except:
                     # initialize
                     self._info = self._read_calo_info_xls(file, show_info=show_info)
-                
+
                 # collect data
                 try:
                     self._data = pd.concat(
-                        [self._data, self._read_calo_data_xls(file, show_info=show_info)])
+                        [
+                            self._data,
+                            self._read_calo_data_xls(file, show_info=show_info),
+                        ]
+                    )
                 except:
                     # initialize
                     self._data = self._read_calo_data_xls(file, show_info=show_info)
@@ -88,15 +93,23 @@ class Measurement():
                 # collect information
                 try:
                     self._data = pd.concat(
-                        [self._data, self._read_calo_data_csv(file, show_info=show_info)])
+                        [
+                            self._data,
+                            self._read_calo_data_csv(file, show_info=show_info),
+                        ]
+                    )
                 except:
                     # initialize
                     self._data = self._read_calo_data_csv(file, show_info=show_info)
-                
+
                 # collect data
                 try:
                     self._info = pd.concat(
-                        [self._info, self._read_calo_info_csv(file, show_info=show_info)])
+                        [
+                            self._info,
+                            self._read_calo_info_csv(file, show_info=show_info),
+                        ]
+                    )
                 except:
                     # initialize
                     self._info = self._read_calo_info_csv(file, show_info=show_info)
@@ -126,7 +139,7 @@ class Measurement():
         """
         # open csv file
         thefile = open(file)
-        # detect empty lines which are characteristic at the beginning and 
+        # detect empty lines which are characteristic at the beginning and
         # end of the data block
         empty_lines = [
             index for index, line in enumerate(csv.reader(thefile)) if len(line) == 0
@@ -156,9 +169,7 @@ class Measurement():
         empty_lines = self._determine_data_range_csv(file)
         # read data from csv-file
         data = pd.read_csv(
-            file, 
-            skiprows=empty_lines[0], 
-            nrows=empty_lines[1] - empty_lines[0] - 2
+            file, skiprows=empty_lines[0], nrows=empty_lines[1] - empty_lines[0] - 2
         )
         # remove "columns" with many NaNs
         data = data.dropna(axis=1, thresh=10)
@@ -166,7 +177,7 @@ class Measurement():
         data.columns = self.colnames
         # add sample name as column
         data["sample"] = file
-        
+
         # return
         return data
 
@@ -192,13 +203,11 @@ class Measurement():
         empty_lines = self._determine_data_range_csv(file)
         # read info block from csv-file
         info = pd.read_csv(
-            file, 
-            nrows=empty_lines[0] - 1, 
-            names=["parameter", "value"]
+            file, nrows=empty_lines[0] - 1, names=["parameter", "value"]
         ).dropna(subset=["parameter"])
         # add sample name as column
         info["sample"] = file
-        # the last block is not really meta data but summary data and 
+        # the last block is not really meta data but summary data and
         # somewhat not necessary
 
         # return
@@ -230,9 +239,7 @@ class Measurement():
         try:
             # get experiment info (first sheet)
             df_experiment_info = xl.parse(
-                sheet_name="Experiment info",
-                header=0,
-                names=["parameter", "value"]
+                sheet_name="Experiment info", header=0, names=["parameter", "value"]
             ).dropna(subset=["parameter"])
             # use first row as header
             df_experiment_info = df_experiment_info.iloc[1:, :]
@@ -274,7 +281,7 @@ class Measurement():
 
         # define Excel file
         xl = pd.ExcelFile(file)
-        
+
         try:
             # get experiment info (first sheet)
             df_data = xl.parse(xl.sheet_names[-1], header=None)
@@ -353,9 +360,16 @@ class Measurement():
     #
     # plot
     #
-    def plot(self, t_unit="h", y="normalized_heat_flow", y_unit_milli=True, regex=None, show_info=True):
+    def plot(
+        self,
+        t_unit="h",
+        y="normalized_heat_flow",
+        y_unit_milli=True,
+        regex=None,
+        show_info=True,
+    ):
         """
-        plot  of 
+        plot  of
             - normalizedheatflow
             - normalizedheat
 
@@ -383,11 +397,11 @@ class Measurement():
         if t_unit == "s":
             x_factor = 1
         elif t_unit == "min":
-            x_factor = 1/60
+            x_factor = 1 / 60
         elif t_unit == "h":
-            x_factor = 1/(60*60)
+            x_factor = 1 / (60 * 60)
         elif t_unit == "d":
-            x_factor = 1/(60*60*24)
+            x_factor = 1 / (60 * 60 * 24)
 
         # y-unit
         if y_unit_milli:
@@ -404,13 +418,15 @@ class Measurement():
                     continue
             # plot
             plt.plot(
-                d["time"]*x_factor,
-                d[y_column]*y_factor,
-                label=os.path.basename(d.loc[0, "sample"]).split(".xls")[0]
+                d["time"] * x_factor,
+                d[y_column] * y_factor,
+                label=os.path.basename(d.loc[0, "sample"])
+                .split(".xls")[0]
+                .split(".csv")[0],
             )
 
         # legend
-        plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False)
+        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5), frameon=False)
 
         # limits
         plt.xlim(left=0)
@@ -434,20 +450,18 @@ class Measurement():
 
         def apllicable(df, target_h=4, cutoff_min=None):
             # convert target time to seconds
-            target_s = 3600*target_h
+            target_s = 3600 * target_h
             # get heat at target time
             hf_at_target = float(
-                df.query("time >= @target_s").
-                head(1)["normalized_heat"]
+                df.query("time >= @target_s").head(1)["normalized_heat"]
             )
 
             # if cuoff time specified
             if cutoff_min:
                 # convert target time to seconds
-                target_s = 60*cutoff_min
+                target_s = 60 * cutoff_min
                 hf_at_cutoff = float(
-                    df.query("time <= @target_s").
-                    tail(1)["normalized_heat"]
+                    df.query("time <= @target_s").tail(1)["normalized_heat"]
                 )
                 # correct heatflow for heatflow at cutoff
                 hf_at_target = hf_at_target - hf_at_cutoff
@@ -456,13 +470,11 @@ class Measurement():
             return hf_at_target
 
         # groupby
-        results = self._data.groupby(by="sample").apply(
-            lambda x: apllicable(
-                x,
-                target_h=target_h,
-                cutoff_min=cutoff_min
-            )
-        ).reset_index(level=0)
+        results = (
+            self._data.groupby(by="sample")
+            .apply(lambda x: apllicable(x, target_h=target_h, cutoff_min=cutoff_min))
+            .reset_index(level=0)
+        )
         # rename
         results.columns = ["sample", "cumulated_heat_at_hours"]
         results["target_h"] = target_h
@@ -478,6 +490,12 @@ class Measurement():
     def get_data(self):
         """
         get data
+
+        Returns
+        -------
+        pd.DataFrame
+            data, i.e. heat flow, heat, sample, ....
+
         """
 
         return self._data
@@ -489,6 +507,12 @@ class Measurement():
     def get_information(self):
         """
         get information
+
+        Returns
+        -------
+        pd.DataFrame
+            information, i.e. date of measurement, operator, comment ...
+
         """
 
         return self._info
