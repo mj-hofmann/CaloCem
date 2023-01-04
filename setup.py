38aa89651aa61e6b3558486e31593a329ac6d208
Matthias Hofmann <matthias.j.hofmann@gmx.de>
2023-01-04 15:43:13 +0100

    moved to toml from setup files

--- setup.py
+++
@@ -1,9 +0,0 @@
-# this file allows installing the module in "editable mode"
-# see https://www.youtube.com/watch?v=DhUpxWjOhME (~ at 6:30)
-# "pip install -e ." see https://www.youtube.com/watch?v=DhUpxWjOhME (~ at 7:50)
-
-from setuptools import setup
-
-if __name__ == "__main__":
-    # use setup function
-    setup()
