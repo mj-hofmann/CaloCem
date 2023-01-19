fb57fde5ab18ea55260070375b1751970c6ad214
mj-hofmann <matthias.j.hofmann@gmx.de>
2023-01-19 21:07:56 +0100

    Update test_last_time_entry.py

--- tests/test_last_time_entry.py
+++ tests/test_last_time_entry.py
@@ -29,7 +29,7 @@ import os
         # ("Mix_06.xls", 85080),
         # ("Mix_07.xls", 9960),
         # ("Mix_08.xls", 336480),
-        ("opc_3.csv", 322470),
+        ("opc_3.csv", 322461),
         # ("c3a.csv", 173929),
         # ("OPC_1.xls", 499440),
         ("OPC_2.xls", 336000),
@@ -39,7 +39,7 @@ import os
 def test_last_time_entry(test_input, expected):
 
     # path to data
-    path_to_data = os.getcwd() + os.sep + "DATA"
+    path_to_data = os.getcwd() + os.sep + os.pardir + os.sep + "DATA"
     # path_to_data = os.getcwd() + os.sep + os.pardir + os.sep + "DATA"
 
     # experiments via class
@@ -57,4 +57,5 @@ def test_last_time_entry(test_input, expected):
     assert last_time == int(expected)
 
 
-# test_last_time_entry("Exp_1.xls", 253680)
+# test_last_time_entry("opc_3.csv", 322461)
+# test_last_time_entry("OPC_2.xls", 336000)
