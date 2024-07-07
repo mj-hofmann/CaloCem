681d06c879a5c5493278da4c7f5e6c8875013987
torben <torben.gaedt@tum.de>
2024-07-07 12:24:58 +0200

    extended tian docs and example py script on tian correction

--- docs/tian.md
+++ docs/tian.md
@@ -1,14 +1,10 @@
 
 
-<<<<<<< HEAD
 # Tian Correction
 
-## Example Workflow Two Tau Constants
+## Example Workflow 
 
 ### Load the Data
-=======
-## Tian Correction
->>>>>>> origin/main
 
 For fast reactions, e.g., reactions occuring during the first minutes of cement hydration, the thermal inertia of the calorimeter significantly broadens the heat flow signal. 
 If the characteristic time constants are determined experimentally, a Tian correction can be applied to the heat flow data.
@@ -133,25 +129,58 @@ In pratical terms, if only the attribute tau1 is set, only the first derivative 
 
 # Set Proceesing Parameters
 processparams = ta.ProcessingParameters()
-processparams.time_constants.tau1 = 240
+processparams.time_constants.tau1 = 300
 ```
+
+The difference between having one or two tau constants can be seen in the following plot.
+In general, having two tau constants renders the signal even more narrow.
+
+![Tian Correction no smoothing](assets/tian_correction_one_tau.png)
 
 
 ## Smoothing the Data
 
+
+### No smoothing
 It is important to smoothen the data. 
 Otherwise, small noise in the raw heat flow data will lead to significant noise especially in the second derivative.
-
+By default the smoothing is no applied. 
+Here, we explicitly set the attributes to `False` and repeat the analysis as shown above.
+The results demonstrates the noise in the data which originates from tiny fluctuations which result in significant noise in the second derivative.
 
 ```python
 
 # Set Proceesing Parameters
-processparams = ta.ProcessingParameters()
-processparams.time_constants.tau1 = 240
-processparams.time_constants.tau2 = 80 
+processparams.median_filter.apply = False
+processparams.spline_interpolation.apply = False
+```
+
+
+![Tian Correction no smoothing](assets/tian_correction_no_smoothing.png)
+
+### Only Median Filter
+
+Here is the result of only applying a [median filter](https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.median_filter.html) with a size of 15.
+
+```python
+
+# Set Proceesing Parameters
 processparams.median_filter.apply = True
-processparams.median_filter.size = 15
+processparams.spline_interpolation.apply = 15
+```
+![Tian Correction no smoothing](assets/tian_correction_median_smoothing.png)
+
+### Only Spline Smoothing
+
+Here is the result of only applying a [Univariate Spline](https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.UnivariateSpline.html#scipy.interpolate.UnivariateSpline) with smmothing of 1e-10 for both the first and the second derivative.
+The combination of a median filter and spline smoothing reliably delivers smooth data without introducing artifacts or significant line broadening.
+
+```python
+
+# Set Proceesing Parameters
+processparams.median_filter.apply = False
 processparams.spline_interpolation.apply = True
 processparams.spline_interpolation.smoothing_1st_deriv = 1e-10
 processparams.spline_interpolation.smoothing_2nd_deriv = 1e-10
 ```
+![Tian Correction no smoothing](assets/tian_correction_spline_smoothing.png)
--- examples/example_tian_correction_params.py
+++ examples/example_tian_correction_params.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import TAInstCalorimetry.tacalorimetry as ta
 
 datapath = Path(__file__).parent.parent / "TAInstCalorimetry" / "DATA"
-plotpath = Path(__file__).parent.parent / "docs" / "assets" 
+plotpath = Path(__file__).parent.parent / "docs" / "assets"
 
 # experiments via class
 tam = ta.Measurement(
@@ -15,7 +15,6 @@ tam = ta.Measurement(
     auto_clean=False,
     cold_start=True,
 )
-
 
 
 # Set Proceesing Parameters
@@ -33,9 +32,11 @@ tam.apply_tian_correction(
     processparams=processparams,
 )
 
+df = tam.get_data().copy()
+
 # plot corrected and uncorrected data
 fig, ax = plt.subplots()
-for name, group in tam._data.groupby("sample_short"):
+for name, group in df.groupby("sample_short"):
     ax.plot(
         group["time_s"] / 60,
         group["normalized_heat_flow_w_g"],
@@ -56,3 +57,148 @@ ax.legend()
 
 plt.savefig(plotpath / "tian_correction.png")
 
+
+# example with only one tau constant and no smoothing
+processparams = ta.ProcessingParameters()
+processparams.time_constants.tau1 = 300
+processparams.time_constants.tau2 = None
+processparams.median_filter.apply = False
+processparams.spline_interpolation.apply = False
+
+tam.apply_tian_correction(processparams=processparams)
+
+df_tau = tam.get_data().copy()
+
+
+fig, ax = plt.subplots()
+for (name, group), (name2, group2) in zip(df_tau.groupby("sample_short"), df.groupby("sample_short")):
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g"],
+        #alpha=0.5,
+        #linestyle="--",
+        #label=name[-2:],
+        label="no Tian, original"
+    )
+    ax.plot(
+        group2["time_s"] / 60,
+        group2["normalized_heat_flow_w_g_tian"],
+        #color="black",
+        label="one tau",
+    )
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g_tian"],
+        #color=ax.get_lines()[-1].get_color(),
+        # label=name[-2:] + " Tian",
+        label="tau1, tau2"
+    )
+ax.set_xlim(0, 15)
+ax.set_xlabel("Time (min)")
+ax.set_ylabel("Normalized Heat Flow (W/g)")
+ax.legend()
+ax.set_title("only one tau constant")
+plt.savefig(plotpath / "tian_correction_one_tau.png")
+
+
+# example without smoothing
+
+processparams.time_constants.tau1 = 240
+processparams.time_constants.tau2 = 80
+processparams.median_filter.apply = False
+processparams.spline_interpolation.apply = False
+
+tam.apply_tian_correction(processparams=processparams)
+
+df2 = tam.get_data()
+# plot corrected and uncorrected data
+fig, ax = plt.subplots()
+for name, group in df2.groupby("sample_short"):
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g"],
+        alpha=0.5,
+        linestyle="--",
+        label=name[-2:],
+    )
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g_tian"],
+        color=ax.get_lines()[-1].get_color(),
+        label=name[-2:] + " Tian",
+    )
+ax.set_xlim(0, 15)
+ax.set_xlabel("Time (min)")
+ax.set_ylabel("Normalized Heat Flow (W/g)")
+ax.legend()
+ax.set_title("No smoothing applied")
+plt.savefig(plotpath / "tian_correction_no_smoothing.png")
+
+
+# example with only median smoothing
+processparams.time_constants.tau1 = 240
+processparams.time_constants.tau2 = 80
+processparams.median_filter.apply = True
+processparams.median_filter.size = 15
+
+tam.apply_tian_correction(processparams=processparams)
+
+df3 = tam.get_data()
+
+fig, ax = plt.subplots()
+for name, group in df3.groupby("sample_short"):
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g"],
+        alpha=0.5,
+        linestyle="--",
+        label=name[-2:],
+    )
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g_tian"],
+        color=ax.get_lines()[-1].get_color(),
+        label=name[-2:] + " Tian",
+    )
+ax.set_xlim(0, 15)
+ax.set_xlabel("Time (min)")
+ax.set_ylabel("Normalized Heat Flow (W/g)")
+ax.legend()
+ax.set_title("Median Filter applied")
+plt.savefig(plotpath / "tian_correction_median_smoothing.png")
+
+
+# Only Univariate Spline Smoothing
+
+processparams.time_constants.tau1 = 240
+processparams.time_constants.tau2 = 80
+processparams.median_filter.apply = False
+processparams.spline_interpolation.apply = True
+processparams.spline_interpolation.smoothing_1st_deriv = 1e-10
+processparams.spline_interpolation.smoothing_2nd_deriv = 1e-10
+
+tam.apply_tian_correction(processparams=processparams)
+
+df4 = tam.get_data()
+
+fig, ax = plt.subplots()
+for name, group in df4.groupby("sample_short"):
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g"],
+        alpha=0.5,
+        linestyle="--",
+        label=name[-2:],
+    )
+    ax.plot(
+        group["time_s"] / 60,
+        group["normalized_heat_flow_w_g_tian"],
+        color=ax.get_lines()[-1].get_color(),
+        label=name[-2:] + " Tian",
+    )
+ax.set_xlim(0, 15)
+ax.set_xlabel("Time (min)")
+ax.set_ylabel("Normalized Heat Flow (W/g)")
+ax.legend()
+ax.set_title("Spline Smoothing applied")
+plt.savefig(plotpath / "tian_correction_spline_smoothing.png")
