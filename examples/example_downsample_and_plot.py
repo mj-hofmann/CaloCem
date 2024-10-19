8203568f94c6bcf4849baa76011c209bd9c59115
torben <torben.gaedt@tum.de>
2024-10-19 21:10:12 +0200

    fixed bug in adaptive downsampling

--- examples/example_downsample_and_plot.py
+++ examples/example_downsample_and_plot.py
@@ -10,7 +10,7 @@ plotpath = parentfolder / "docs" / "assets"
 testpath = parentfolder / "tests"
 
 processparams = ta.ProcessingParameters()
-processparams.downsample.apply = False
+processparams.downsample.apply = True
 processparams.downsample.num_points = 300
 processparams.downsample.section_split = True
 processparams.downsample.section_split_time_s = 3600
@@ -19,7 +19,8 @@ processparams.downsample.baseline_weight = 0.1
 # experiments via class
 tam_d = ta.Measurement(
     folder=datapath,
-    regex=r".*data_[1-2].csv",
+    # regex=r".*data_[1-2].csv",
+    regex="downsample.*",
     show_info=True,
     auto_clean=False,
     cold_start=True,
@@ -28,23 +29,14 @@ tam_d = ta.Measurement(
 
 tam = ta.Measurement(
     folder=datapath,
-    regex=r".*data_[1-2].csv",
+    # regex=r".*data_[1-2].csv",
+    regex="downsample.*",
     show_info=True,
     auto_clean=False,
     cold_start=True,
 )
 
-#%%
-fig, ax = plt.subplots()
-for name, group in tam_d._data.groupby("sample_short"):
-    ax.plot(group["time_s"]/3600, group["normalized_heat_flow_w_g"]*1000, "x-", label=name)
-    
-ax.set_xlim(0,24)
-ax.set_ylim(0,25)
-plt.show()
 
-# %%
-print(len(tam._data))
 # %%
 
 fig, ax = plt.subplots()
@@ -54,7 +46,7 @@ for (name, group), (name2, group2) in zip(tam._data.groupby("sample_short"), tam
 # ax.plot(tam._data["time_s"]/3600, tam._data["normalized_heat_flow_w_g"]*1000, "-", label="original")
 # ax.plot(tam_d._data["time_s"]/3600, tam_d._data["normalized_heat_flow_w_g"]*1000, "-", label="downsampled")
 ax.legend()
-ax.set_xlim(0,10)
-ax.set_ylim(0,4)
+# ax.set_xlim(0,10)
+# ax.set_ylim(0,4)
 
 # %%
